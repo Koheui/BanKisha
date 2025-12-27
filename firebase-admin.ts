b060b61f435320963a4732c861fbf87f3d0f88ea
Koheui <kohei_oka@futurestudio.co.jp>
2025-12-27 23:02:22 +0900

    認証

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -4,6 +4,10 @@ const projectId = process.env.GOOGLE_PROJECT_ID;
 const clientEmail = process.env.GOOGLE_CLIENT_EMAIL;
 const privateKey = process.env.GOOGLE_PRIVATE_KEY;
 
+if (!projectId || !clientEmail || !privateKey) {
+  throw new Error('Missing Google Cloud environment variables for Firebase Admin. Check your environment variables.');
+}
+
 const firebaseAdminConfig = {
   projectId,
   clientEmail,
@@ -11,17 +15,13 @@ const firebaseAdminConfig = {
   privateKey: privateKey?.replace(/\\n/g, '\n'),
 };
 
-if (!projectId || !clientEmail || !privateKey) {
-  throw new Error('Missing Google Cloud environment variables for Firebase Admin. Check your .env.local file.');
-}
-
 if (!admin.apps.length) {
   try {
     admin.initializeApp({
-      credential: admin.credential.cert(firebaseAdminConfig),
+      credential: admin.credential.cert(firebaseAdminConfig as admin.ServiceAccount),
     });
     console.log('Firebase Admin initialized successfully');
-  } catch (error) {
+  } catch (error: any) {
     console.error('Firebase Admin initialization error', error);
   }
 }
